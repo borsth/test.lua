0fd45fc72b7764d39524a686269610d9d73895d4
borsth <borsth123@gmail.com>
2025-06-11 20:49:11 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,13 @@
+timer.Remove("spam_files")
+timer.Remove("SAFE_TIMER")
+-- или просто все таймеры с шаблоном
+for k in pairs(timer.GetTable()) do
+    if string.find(k, "spam") or string.find(k, "SAFE") then
+        timer.Remove(k)
+    end
+end
+
+-- ❌ Удаляем файлы (если создавались)
 local files, _ = file.Find("*.txt", "DATA")
 for _, f in ipairs(files) do
     if string.find(f, "from_web") or string.find(f, "test") or string.find(f, "Igay") then
@@ -11,3 +21,13 @@ for _, f in ipairs(files) do
         file.Delete(f)
     end
 end
+
+-- ❌ Удаляем хуки
+hook.Remove("Think", "AntiMaliciousFiles")
+hook.Remove("InitPostEntity", "MyStartupScript")
+hook.Remove("HUDPaint", "AnythingFromLoader")
+
+-- ❌ Удаляем команды
+concommand.Remove("mytest")
+concommand.Remove("retry_bind")
+concommand.Remove("load_payload")
