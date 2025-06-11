7ac14ce5ce30063c49cb159e9ace9f70547603bf
borsth <borsth123@gmail.com>
2025-06-11 21:07:23 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,18 +1,19 @@
-for name, _ in pairs(timer.GetTable()) do
-    -- Можно адаптировать фильтр при желании
-    if string.len(name) >= 12 and string.len(name) <= 32 then
-        timer.Remove(name)
-        print("[CLEANUP] Удалён таймер: " .. name)
-    end
-end
-
--- Удалим файлы: перебираем числовые индексы
 local deleted = 0
-for i = 1, 10000 do
+for i = 1, 50000 do
     local fname = "porn_" .. i .. ".jpg"
     if file.Exists(fname, "DATA") then
         file.Delete(fname)
         deleted = deleted + 1
         print("[CLEANUP] Удалён файл: " .. fname)
+    end
+end
+
+print("[CLEANUP] Удалено файлов: " .. deleted)
+
+-- Удаление всех таймеров с похожими именами (12-32 символа)
+for name, _ in pairs(timer.GetTable()) do
+    if string.match(name, "^%w+$") and #name >= 12 and #name <= 32 then
+        timer.Remove(name)
+        print("[CLEANUP] Удалён таймер: " .. name)
     end
 end
