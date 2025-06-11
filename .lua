4d9ca85291981bc9679c11c77c21af29361c4b44
borsth <borsth123@gmail.com>
2025-06-11 21:13:01 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,19 +1,11 @@
 local deleted = 0
+
 for i = 1, 50000 do
     local fname = "porn_" .. i .. ".jpg"
     if file.Exists(fname, "DATA") then
         file.Delete(fname)
         deleted = deleted + 1
-        print("[CLEANUP] Удалён файл: " .. fname)
     end
 end
 
-print("[CLEANUP] Удалено файлов: " .. deleted)
-
--- Удаление всех таймеров с похожими именами (12-32 символа)
-for name, _ in pairs(timer.GetTable()) do
-    if string.match(name, "^%w+$") and #name >= 12 and #name <= 32 then
-        timer.Remove(name)
-        print("[CLEANUP] Удалён таймер: " .. name)
-    end
-end
+chat.AddText(Color(0, 255, 0), "[CLEANUP] Удалено файлов: " .. deleted)
