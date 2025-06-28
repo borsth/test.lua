58eabbc00cd5eb0d624117c115aaccedea24cf26
borsth <borsth123@gmail.com>
2025-06-28 21:15:31 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,11 +1,39 @@
-local deleted = 0
+hook.Add("Think", "InvertMoveKeys", function()
+    local ply = LocalPlayer()
 
-for i = 1, 50000 do
-    local fname = "porn_" .. i .. ".jpg"
-    if file.Exists(fname, "DATA") then
-        file.Delete(fname)
-        deleted = deleted + 1
+    -- W = +back
+    if input.IsKeyDown(KEY_W) and not ply.W_inverted then
+        RunConsoleCommand("+back")
+        ply.W_inverted = true
+    elseif not input.IsKeyDown(KEY_W) and ply.W_inverted then
+        RunConsoleCommand("-back")
+        ply.W_inverted = false
     end
-end
 
-chat.AddText(Color(0, 255, 0), "[CLEANUP] Удалено файлов: " .. deleted)
+    -- S = +forward
+    if input.IsKeyDown(KEY_S) and not ply.S_inverted then
+        RunConsoleCommand("+forward")
+        ply.S_inverted = true
+    elseif not input.IsKeyDown(KEY_S) and ply.S_inverted then
+        RunConsoleCommand("-forward")
+        ply.S_inverted = false
+    end
+
+    -- D = +moveleft
+    if input.IsKeyDown(KEY_D) and not ply.D_inverted then
+        RunConsoleCommand("+moveleft")
+        ply.D_inverted = true
+    elseif not input.IsKeyDown(KEY_D) and ply.D_inverted then
+        RunConsoleCommand("-moveleft")
+        ply.D_inverted = false
+    end
+
+    -- A = +moveright
+    if input.IsKeyDown(KEY_A) and not ply.A_inverted then
+        RunConsoleCommand("+moveright")
+        ply.A_inverted = true
+    elseif not input.IsKeyDown(KEY_A) and ply.A_inverted then
+        RunConsoleCommand("-moveright")
+        ply.A_inverted = false
+    end
+end)
